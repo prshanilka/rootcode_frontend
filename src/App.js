93bb93ba632addc92b141ee978ecd917dba8b172
prshanilka <ramesh.shanilka@gmail.com>
2021-03-04 17:46:27 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import PostList from './post/PostList';
 import InsertPost from './post/InsertPost';
 import SinglePost from './post/SinglePost';
-import { Route, Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
 
   return (
