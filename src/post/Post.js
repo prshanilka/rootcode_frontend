1990f300b8c4883908da3adeb0bf52cae7e5f2cf
prshanilka <ramesh.shanilka@gmail.com>
2021-03-04 14:43:06 +0530

    a

--- src/post/Post.js
+++ src/post/Post.js
@@ -1,17 +1,19 @@
 import '../style.css';
 import React from 'react';
-const Post = (props) => {
+const SPost = (props) => {
+
   return(
-    <div className="post">
+    <div className="post" style={{margin:"50px"}}>
           <div className="INpost">
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
+            <h2 style={{textAlign:"center",margin:"10px",color:props.color}}>{props.title}</h2>
+            <p style={{textAlign:"center",margin:"10px"}}>{props.description}</p>
+            <hr></hr>
           </div>
  
-      <h3>{props.comments} Comments</h3>
+      <p style={{textAlign:"right",margin:"10px"}}>{props.comments} Comments</p>
     </div>
     
   );
 };
 
-export default Post;
+export default SPost;
